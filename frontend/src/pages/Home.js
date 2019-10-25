6c978b9973b836fc263bec612ed929d25bee9fd4
Sunny Wong <sunnywong18@gmail.com>
2019-10-25 10:14:49 +0200

    finished setting local storage for customer signin, singup

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link, Route } from "react-router-dom"
 import "../Home.scss";
-
+import dogwalker from "../images/dogwalker"
 
 export default class Home extends Component {
   constructor(props) {
@@ -18,7 +18,7 @@ export default class Home extends Component {
             <option value="Professional">Professional</option>
           </select>and im in the IT field
         </h1>
-
+        <img src={dogwalker} alt="" />
         <button>Get A Quote Now!</button>
       </div>
     );
