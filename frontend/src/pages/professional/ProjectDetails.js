5bdf1da2917966777dd20165d27654baf6ede67e
Edward Lee <eddy2706@gmail.com>
2019-10-26 19:49:12 +0200

    remove form once is submited

--- frontend/src/pages/professional/ProjectDetails.js
+++ frontend/src/pages/professional/ProjectDetails.js
@@ -1,31 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { getUser } from "../../utils/auth";
-
-// export default function ProjectDetails(props) {
-//   const projectId = props.match.params.id;
-//   let filterproject = props.projects.filter(
-//     project => project._id === projectId
-//   )[0];
-//     debugger
-//   return (
-//     <>
-//     {props.projects.length ?
-//       <div>
-//       <h1>{filterproject.title}</h1>
-//     </div>:
-//     <h1>Loading</h1>
-//     }
-//     </>
-//   );
-// }
-
+import Prodetail from "./Prodetail";
 export default class ProjectDetails extends Component {
   state = {
     project: null,
     hourlyPrice: "",
     description: "",
-    user: getUser()
+    user: getUser(),
+    quote: null
   };
   componentDidMount() {
     this.fetchProjectDetails();
@@ -37,10 +20,25 @@ export default class ProjectDetails extends Component {
       url: `http://localhost:5000/api/projects/${this.props.match.params.id}`
     })
       .then(res => {
-        this.setState({ project: res.data });
+        let check = res.data.quotes.filter(
+          element => element.professional === this.state.user.id
+        );
+        if (check.length > 0) {
+          this.setState({
+            project: res.data,
+            quote: check[0]
+          });
+        } else {
+          this.setState({
+            project: res.data,
+            hourlyPrice: "",
+            description: "",
+            quote: null
+          });
+        }
       })
       .catch(err => {
-        this.setState(err);
+        console.log(err);
       });
   }
 
@@ -69,7 +67,7 @@ export default class ProjectDetails extends Component {
       url: "http://localhost:5000/api/quotes/create"
     })
       .then(response => {
-        console.log(response);
+        this.fetchProjectDetails();
       })
       .catch(error => {
         console.log(error);
@@ -81,32 +79,55 @@ export default class ProjectDetails extends Component {
       <div className="project-detail-container">
         {this.state.project ? (
           <>
-            <h1>{this.state.project.title}</h1>
-            <h3>{this.state.project.description}</h3>
-            <h3>{this.state.project.category}</h3>
+            <Prodetail project={this.state.project} />
 
-            <form onSubmit={this.sumbitHandler}>
-              <input
-                placeholder="Price per hour"
-                onChange={this.inputHandler}
-                name="hourlyPrice"
-                value={this.state.hourlyPrice}
-                type="text"
-              />
-              <input
-                placeholder="Description"
-                onChange={this.inputHandler}
-                name="description"
-                value={this.state.description}
-                type="text"
-              />
-              <button type="submit"> Sumbit Quote</button>
-            </form>
+            {this.state.quote ? (
+              <>
+                <h1>{this.state.quote.hourlyPrice} </h1>
+                <h1>im here</h1>
+              </>
+            ) : (
+              <form onSubmit={this.sumbitHandler}>
+                <input
+                  placeholder="Price per hour"
+                  onChange={this.inputHandler}
+                  name="hourlyPrice"
+                  value={this.state.hourlyPrice}
+                  type="text"
+                />
+                <input
+                  placeholder="Description"
+                  onChange={this.inputHandler}
+                  name="description"
+                  value={this.state.description}
+                  type="text"
+                />
+                <button type="submit"> Sumbit Quote</button>
+              </form>
+            )}
           </>
         ) : (
-          <h1>Loading</h1>
+          <div className="loading"></div>
         )}
       </div>
     );
   }
 }
+
+// export default function ProjectDetails(props) {
+//   const projectId = props.match.params.id;
+//   let filterproject = props.projects.filter(
+//     project => project._id === projectId
+//   )[0];
+//     debugger
+//   return (
+//     <>
+//     {props.projects.length ?
+//       <div>
+//       <h1>{filterproject.title}</h1>
+//     </div>:
+//     <h1>Loading</h1>
+//     }
+//     </>
+//   );
+// }
