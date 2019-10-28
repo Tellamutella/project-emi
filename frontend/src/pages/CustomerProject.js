61980f8dc936b940c4d1cb1b34e6af657f6d4eb7
Edward Lee <eddy2706@gmail.com>
2019-10-28 21:53:40 +0100

    added css for homepage

--- frontend/src/pages/CustomerProject.js
+++ frontend/src/pages/CustomerProject.js
@@ -1,57 +1,54 @@
-import React, { Component } from 'react'
-import { getUser, getCustomer } from "../utils/auth"
+import React, { Component } from "react";
+import { getUser, getCustomer } from "../utils/auth";
 import { Link } from "react-router-dom";
-import "./CusProject.scss"
-import BasicLayout from '../layout/BasicLayout';
+import "./CusProject.scss";
+import BasicLayout from "../layout/BasicLayout";
 import { getProjects } from "../utils/projects";
 
 export default class CustomerProject extends Component {
+  state = {
+    projects: [],
+    filteredProjects: [],
+    user: getCustomer()
+  };
 
-    state = {
-        projects: [],
-        filteredProjects: [],
-        user: getCustomer()
-    }
+  filterProject = data => {
+    var matchResult = data.filter(
+      project => project.customer === this.state.user.id
+    );
+    this.setState({ filteredProjects: matchResult });
+  };
 
-    filterProject = (data) => {
-        var matchResult = data.filter((project) =>
-            project.customer === this.state.user.id)
-        this.setState({ filteredProjects: matchResult })
-    }
+  componentDidMount() {
+    getProjects()
+      .then(project => {
+        this.setState({ projects: project.data });
+        this.filterProject(project.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-
-    componentDidMount() {
-
-        getProjects()
-            .then((project) => {
-                this.setState({ projects: project.data })
-                this.filterProject(project.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-    }
-
-    render() {
-        return (
-            <BasicLayout>
-                <>
-                    <div className="CusPro">
-                        {this.state.filteredProjects.map((project) =>
-                            <div className="CusProBox">
-                                <div className="CusProBoxDiv">
-                                    <h2>{project.title}</h2>
-                                    <p>Quotes received: {project.quotes.length}</p>
-                                </div>
-                                <Link to={`/customer/projects/${project._id}`}>
-                                    <button>Check quotes</button>
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                </>
-            </BasicLayout>
-        )
-    }
+  render() {
+    return (
+      <BasicLayout>
+        <>
+          <div className="CusPro">
+            {this.state.filteredProjects.map(project => (
+              <div className="CusProBox">
+                <div className="CusProBoxDiv">
+                  <h2>{project.title}</h2>
+                  <p>Quotes received: {project.quotes.length}</p>
+                </div>
+                <Link to={`/customer/projects/${project._id}`}>
+                  <button>Check quotes</button>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </>
+      </BasicLayout>
+    );
+  }
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,70 @@
-import React, { Component } from "react";
 import "./Home.scss";
 import dogsitter from "../images/dogsitter.svg";
-import BasicLayout from "../layout/BasicLayout"
-const Home = () => {
+import BasicLayout from "../layout/BasicLayout";
+import React, { Component } from "react";
 
-  debugger
-  return (
-    <BasicLayout>
-      <div className="home">
-        <div className="homeColRow">
-          <h1>
-            I am a<span></span>
-            <select className="selection">
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "Customer",
+      input: "Home Services"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+  handleSelect = event => this.setState({ user: event.target.value });
+  handleInput = event => this.setState({ input: event.target.value });
+
+  render() {
+    return (
+      <BasicLayout>
+        <div className="home-container">
+          <div className="text-container">
+            <h1>I am a</h1>
+            <select
+              className="selection"
+              value={this.state.user}
+              onChange={this.handleSelect}
+            >
               <option value="Customer">Customer</option>
               <option value="Professional">Professional</option>
             </select>
-            and im in the IT field
-        </h1>
-          <button>Get A Quote</button>
+            {this.state.user === "Professional" ? (
+              <>
+                <h1> and im in the</h1>
+                <select
+                  className="selection"
+                  value={this.state.input}
+                  onChange={this.handleInput}
+                >
+                  <option value="IT">IT</option>
+                  <option value="Pet">Pet</option>
+                  <option value="Home Services">Home Services</option>
+                  <option value="Fitness">Fitness</option>
+                </select>
+                <h1>Field</h1>
+              </>
+            ) : (
+              <>
+                <h1> and im looking for</h1>
+                <select
+                  className="selection"
+                  value={this.state.input}
+                  onChange={this.handleInput}
+                >
+                  <option value="IT">IT</option>
+                  <option value="Pet">Pet</option>
+                  <option value="Home Services">Home Services</option>
+                  <option value="Fitness">Fitness</option>
+                </select>
+                <h1>Services</h1>
+                <button>Get A Quote</button>
+              </>
+            )}
+          </div>
         </div>
-        <img src={dogsitter} alt="" />
-      </div>
-    </BasicLayout>
-  )
+      </BasicLayout>
+    );
+  }
 }
-
-export default Home;
