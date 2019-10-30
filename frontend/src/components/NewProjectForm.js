344a3d221625ee4a1117d93f40b0175313350693
Edward Lee <eddy2706@gmail.com>
2019-10-30 11:27:45 +0100

    project list refresh after customer form submit

--- frontend/src/components/NewProjectForm.js
+++ frontend/src/components/NewProjectForm.js
@@ -1,55 +1,101 @@
-import React, { Component } from 'react'
-import { newProject } from '../utils/projects'
-import "./NewProjectForm.scss"
+import React, { Component } from "react";
+import { newProject, getProjects } from "../utils/projects";
+import "./NewProjectForm.scss";
+import DatePicker from "react-datepicker";
+import { getCustomer } from "../utils/auth";
 
 export default class NewProjectForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: "",
-            category: "",
-            description: "",
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSelect = this.handleSelect.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      category: "Home Services",
+      description: "",
+      location: "",
+      startDate: new Date(),
+      customer: getCustomer()
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        newProject(this.state.title, this.state.category, this.state.description)
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    newProject(
+      this.state.category,
+      this.state.description,
+      this.state.title,
+      this.state.customer.id,
+      this.state.startDate
+    );
+    this.props.fetchData();
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    handleSelect = e => this.setState({ category: e.target.value });
+  dateChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <form className="NewProjectForm" onSubmit={this.handleSubmit}>
-                    <label>Title</label>
-                    <input required onChange={this.handleChange} value={this.state.title} name="title" placeholder="Enter title" />
+  handleSelect = e => this.setState({ category: e.target.value });
 
-                    <label>Experts you are looking for</label>
-                    <select value={this.state.category} onChange={this.handleSelect}>
-                        <option value="IT">IT</option>
-                        <option selected value="Home Services">Home Services</option>
-                        <option value="Pet">Pet</option>
-                        <option value="Fitness">Fitness</option>
-                    </select>
-
-                    <label>Description</label>
-                    <input required onChange={this.handleChange} value={this.state.description} name="description" placeholder="Enter descriptions" />
-
-                    <button type="submit" Value="Submit">Submit</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="form-container">
+        <h3> Add Project</h3>
+        <form className="NewProjectForm" onSubmit={this.handleSubmit}>
+          <div className="input-container">
+            <label>Title</label>
+            <input
+              required
+              onChange={this.handleChange}
+              value={this.state.title}
+              name="title"
+              placeholder="Enter title"
+            />
+          </div>
+          <div className="input-container">
+            <label>Experts you are looking for</label>
+            <select value={this.state.category} onChange={this.handleSelect}>
+              <option value="IT">IT</option>
+              <option selected value="Home Services">
+                Home Services
+              </option>
+              <option value="Pet">Pet</option>
+              <option value="Fitness">Fitness</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label>Description</label>
+            <textarea
+              required
+              onChange={this.handleChange}
+              type="text"
+              value={this.state.description}
+              name="description"
+              placeholder="Enter description"
+            />
+          </div>
+          <label>Pick a date</label>
+          <DatePicker
+            className="date-picker"
+            selected={this.state.startDate}
+            onChange={this.dateChange}
+            minDate={new Date()}
+            name="startDate"
+          />
+          <button className="form-btn" type="submit" Value="Submit">
+            Add Project
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-
--- frontend/src/pages/CustomerProQuotes.js
+++ frontend/src/pages/CustomerProQuotes.js
@@ -1,68 +1,72 @@
-import React, { Component } from 'react'
-import { Link, Route } from "react-router-dom"
-import axios from 'axios'
-import "./CustomerProQuotes.scss"
-import BasicLayout from '../layout/BasicLayout'
-import { getSingleProject } from '../utils/projects'
-import { login, getCustomer } from '../utils/auth'
-
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import axios from "axios";
+import "./CustomerProQuotes.scss";
+import BasicLayout from "../layout/BasicLayout";
+import { getSingleProject } from "../utils/projects";
+import { login, getCustomer } from "../utils/auth";
 
 export default class CustomerProQuotes extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            project: null,
-            customer: getCustomer(),
-        }
-        this.handlelogin = this.handlelogin.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: null,
+      customer: getCustomer()
+    };
+    this.handlelogin = this.handlelogin.bind(this);
+  }
 
+  handlelogin(customerId, professionalId) {
+    this.props.history.push(`/customer/chat/${customerId}/${professionalId}`);
+  }
 
-    handlelogin(customerId, professionalId) {
-        this.props.history.push(`/customer/chat/${customerId}/${professionalId}`)
-    }
+  componentDidMount() {
+    var projectId = this.props.match.params.id;
+    getSingleProject(projectId)
+      .then(response => {
+        this.setState({ project: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    componentDidMount() {
+  render() {
+    const customerId = this.state.customer.id;
 
-        var projectId = this.props.match.params.id
-        getSingleProject(projectId)
-            .then((response) => {
-                this.setState({ project: response.data })
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
-
-    render() {
-        const customerId = this.state.customer.id
-
-        return (
-            <BasicLayout>
-
-                <div>
-                    {this.state.project === null ?
-                        <p>loading </p> :
-                        <div className="QuotesLargeContainer">
-                            <div className="QuotesProjectContainer">
-                                <h2>{this.state.project.title}</h2>
-                                <p>{this.state.project.description}</p>
-                            </div>
-                            {this.state.project.quotes.map((quote) =>
-                                <div className="QuotesContainer">
-                                    <div className="QuotesContainerColRow">
-                                        <h3>Quote from {quote.professional.firstName}</h3>
-                                        <p>Price: ${quote.hourlyPrice}/hour</p>
-                                        <p>{quote.description}</p>
-                                    </div>
-                                    <button onClick={() => { this.handlelogin(customerId, quote.professional._id) }}>Chat</button>
-                                </div>
-                            )}
-                        </div>
-                    }
+    return (
+      <BasicLayout>
+        <div>
+          {this.state.project === null ? (
+            <p>loading </p>
+          ) : (
+            <div className="QuotesLargeContainer">
+              <div className="QuotesProjectContainer">
+                <h2>{this.state.project.title}</h2>
+                <p>{this.state.project.description}</p>
+                <p>{this.state.project.location}</p>
+                <p>{this.state.project.date}</p>
+              </div>
+              {this.state.project.quotes.map(quote => (
+                <div className="QuotesContainer">
+                  <div className="QuotesContainerColRow">
+                    <h3>Quote from {quote.professional.firstName}</h3>
+                    <p>Price: ${quote.hourlyPrice}/hour</p>
+                    <p>{quote.description}</p>
+                  </div>
+                  <button
+                    onClick={() => {
+                      this.handlelogin(customerId, quote.professional._id);
+                    }}
+                  >
+                    Chat
+                  </button>
                 </div>
-
-            </BasicLayout>
-        )
-    }
+              ))}
+            </div>
+          )}
+        </div>
+      </BasicLayout>
+    );
+  }
 }
