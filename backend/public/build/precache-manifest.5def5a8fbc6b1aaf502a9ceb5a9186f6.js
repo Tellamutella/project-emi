828f1eb916c774bb45dfc07debb700f5ed8e2dd9
Edward Lee <eddy2706@gmail.com>
2019-11-06 17:28:45 +0100

    rebuild no 67

---
+++ backend/public/build/precache-manifest.5def5a8fbc6b1aaf502a9ceb5a9186f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2814cc412a21719fcc438710ff5887e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2712e3db51cbf2e1b0ee",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "02e966138e6c8b29377b",
+    "url": "/static/css/main.85685ab2.chunk.css"
+  },
+  {
+    "revision": "2712e3db51cbf2e1b0ee",
+    "url": "/static/js/2.969e3fe2.chunk.js"
+  },
+  {
+    "revision": "02e966138e6c8b29377b",
+    "url": "/static/js/main.e9be143b.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "a03ab23d9600f3009b4f2ce9898d539a",
+    "url": "/static/media/avatar-profile.a03ab23d.bin"
+  },
+  {
+    "revision": "eb4721ba2cc41c59d444fd5e40f4c219",
+    "url": "/static/media/avatar-profile2.eb4721ba.bin"
+  },
+  {
+    "revision": "894e99ada42cc61216f34275f90d55fd",
+    "url": "/static/media/burger-menu-cross.894e99ad.png"
+  },
+  {
+    "revision": "fe75e4b1a03e1ae19ccb8a99a9e62c5a",
+    "url": "/static/media/emilogo5.fe75e4b1.bin"
+  },
+  {
+    "revision": "ca2564a25b8dd715d18712262a9e4e86",
+    "url": "/static/media/female.ca2564a2.svg"
+  },
+  {
+    "revision": "da1461b923030763c06b02db9c1e9589",
+    "url": "/static/media/undraw_team_spirit_hrr4.da1461b9.svg"
+  }
+]);
